6306932e123a5412822630b1f474a09821e0c039
Caio Martins do Nascimento <martinscaio29@gmail.com>
2023-11-01 09:17:22 -0300

    ajeitando a função de mudarContraste

--- ferramentas.js
+++ ferramentas.js
@@ -5,7 +5,7 @@ document.getElementById('closseButton').addEventListener('click', function () {
 
 
 //ação para aumentar a fonte
-document.getElementById('aumentarFonte').addEventListener('click', function () {
+document.getElementById('aumentarFonte').addEventListener('dblclick', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'aumentarFonte' });
     });
