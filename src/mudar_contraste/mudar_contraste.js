d8e94ff02f6f1fd7f95c22d5ed5829e93b467ee3
Caio Martins do Nascimento <martinscaio29@gmail.com>
2023-11-01 16:53:41 -0300

    f

--- src/mudar_contraste/mudar_contraste.js
+++ src/mudar_contraste/mudar_contraste.js
@@ -1,9 +1,27 @@
 //função para mudar o contraste
-document.getElementById('mudarContraste').addEventListener('dblclick', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: 'document.body.style.filter = "invert(1)";' }
-        );
-    });
+document.getElementById('mudarContraste').addEventListener('click', function () {
+
+    let contraste = document.getElementById('mudarContraste').value;
+
+    if(contraste == 0){
+
+        document.getElementById('mudarContraste').value = 1;
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                { code: 'document.body.style.filter = "invert(1)";' }
+            );
+        });
+    }else{
+
+        document.getElementById('mudarContraste').value = 0;
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                { code: 'document.body.style.filter = "";' }
+            );
+        });
+    }
 });
