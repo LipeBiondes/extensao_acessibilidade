229204058355d61755650b9171ad19d24ed6058a
Caio Martins do Nascimento <martinscaio29@gmail.com>
2023-11-02 15:54:01 -0300

    press f

--- src/aumentar_fonte/aumentar_fonte.js
+++ src/aumentar_fonte/aumentar_fonte.js
@@ -1,26 +1,16 @@
-
 //função para aumentar a fonte  
 function aumentarFonte() {
-    var elementosDeTexto = document.querySelectorAll('p, span, div, h1, h2, h3, h4, h5, h6, a, li, td, th');
+    // Obtém todos os elementos de texto na página da web.
+    const elementosDeTexto = document.querySelectorAll('p, span, div, h1, h2, h3, h4, h5, h6, a, li, td, th');
 
-    for (var i = 0; i < elementosDeTexto.length; i++) {
-        var elemento = elementosDeTexto[i];
-        var estilo = window.getComputedStyle(elemento);
-        var tamanhoAtual = parseFloat(estilo.fontSize);
-        elemento.style.fontSize = (tamanhoAtual + 2) + 'px';
-    }
+    // Aumenta o tamanho da fonte de todos os elementos de texto.
+    elementosDeTexto.forEach((elemento) => {
+        const estilo = getComputedStyle(elemento);
+        const tamanhoAtual = parseFloat(estilo.fontSize);
+        elemento.style.fontSize = tamanhoAtual + 2 + 'px';
+    });
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'aumentarFonte') {
-        aumentarFonte();
-    }
-});
+document.getElementById('aumentarFonte').addEventListener('click', aumentarFonte);
 
-//ação para aumentar a fonte
-document.getElementById('aumentarFonte').addEventListener('click', function () {
-  chromeja.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'aumentarFonte' });
-    });
-});
 
