9d239d0b7a77ff89c7eb25fac33abce58d70f9d9
Caio Martins do Nascimento <martinscaio29@gmail.com>
2023-11-03 11:07:24 -0300

    f

--- src/texto_negrito/texto_negrito.js
+++ src/texto_negrito/texto_negrito.js
@@ -16,23 +16,3 @@ document.getElementById('texto_negrito').addEventListener('click', function () {
     });
 });
 
-
-//função para voltar o texto ao normal
-
-function textoNormal() {
-    const elementosDeTexto = document.querySelectorAll('p, span, div, h1, h2, h3, h4, h5, h6, a, li, td, th');
-
-    elementosDeTexto.forEach((elemento) => {
-        elemento.style.fontWeight = 'normal';
-    });
-}
-
-document.getElementById('texto_normal').addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: `(${textoNormal.toString()})();` }
-        );
-    });
-});
-
